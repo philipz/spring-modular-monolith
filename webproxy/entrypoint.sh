209a96c26592a67aa668017c5e3c23d121722462
philipz <philipzheng@gmail.com>
2025-10-09 23:44:37 +0800

    chore(env): add example environment file and validate HYPERDX_API_KEY in entrypoint script

--- webproxy/entrypoint.sh
+++ webproxy/entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 set -e
 
+# Validate required environment variable
+if [ -z "${HYPERDX_API_KEY}" ]; then
+  echo "ERROR: HYPERDX_API_KEY environment variable is not set"
+  exit 1
+fi
+
 # Substitute environment variables in nginx config
 envsubst '${HYPERDX_API_KEY}' < /tmp/nginx.conf.template > /etc/nginx/nginx.conf
 
